36bddfb699b5e9199803abe78b949b1dd7fbc00d
landaida <landaida.1@gmail.com>
2015-09-09 16:58:34 -0400

    first
    Primeras clases

---
+++ PruebaGetMetaById.php
@@ -0,0 +1,11 @@
+?>
+<h1>Consultar por ID</h1> 
+<form action="getMetaById.php" method="aquí va el método"> 
+ID de la meta:
+
+<input type="text" name="idMeta" value=""/> 
+
+
+
+<input type="submit" value="Consultar"/> 
+</form>
---
+++ add.php
@@ -0,0 +1,26 @@
+<html>
+<head><title>Addition php</title></head>
+<body>
+<!-- TESTES
+
+http://localhost/app/getMetaById.php?idMeta=3
+http://localhost/app/obtener_metas.php
+
+-->
+<?php
+  	# operator
+	print "<h2>php program to add two numbers...</h2><br />";
+              $val1 = 20;
+              $val2 = 20;
+              $sum = $val2 + $val2;   /* Assignment operator */
+              echo "Result(SUM): $sum";
+?>
+
+</body>
+</html>
+
+
+
+
+
+
